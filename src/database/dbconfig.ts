582a28e9614afd47e57cd695ec12cedee4263210
Branham Karanja <branham.karanja@outlook.com>
2025-12-09 11:45:48 +0300

    db(config) configure backend for remote Azure SQL connection

--- src/database/dbconfig.ts
+++ src/database/dbconfig.ts
@@ -26,7 +26,7 @@ export const Config = {
       idleTimeoutMillis: 30000,
     },
     options: {
-      encrypt: false,
+      encrypt: true,
       trustServerCertificate: true,
       enableArithAbort: true,
     },
