8c8df8be7b1b8946b2f54b90fa3c5501ef871290
Branham Karanja <branham.karanja@outlook.com>
2025-12-10 16:44:23 +0300

    fix: update environment variable name for server socket configuration

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ dotenv.config();
 
 
 
-const PORT = process.env.SOCKET ? parseInt(process.env.SOCKET) : 4600;
+const PORT = process.env.SERVER_SOCKET ? parseInt(process.env.SERVER_SOCKET) : 4600;
 
 const app = new Hono()
 
